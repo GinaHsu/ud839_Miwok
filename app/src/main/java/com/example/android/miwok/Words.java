1b68062866cf856dd09859e1363c9ad89540ae37
Gina <iamyuchinghsu@gmail.com>
2017-04-20 17:47:19 -0700

    add Taiwanese language translation

--- app/src/main/java/com/example/android/miwok/Words.java
+++ app/src/main/java/com/example/android/miwok/Words.java
@@ -1,29 +1,37 @@
-package com.example.android.miwok;
-
 /**
  * Created by Gina on 4/18/17.
+ *
+ * Create a new Words object and its constructor
+ *
+ *  @param miwokTranslation is the word in the Miwok language
+ *  @param defaultTranslation is the word in a language that the user
+ *                            is already familiar with
+ *  @param TWTranslation is the word in the Taiwan language
  */
 
+package com.example.android.miwok;
+
+
 public class Words {
-    String eng;
-    String miwok;
+    private String defaultTranslation;
+    private String miwokTranslation;
+    private String TWTranslation;
 
-    public Words(String miwokW, String engW){
-        miwok = miwokW;
-        eng = engW;
+    public Words(String miwokW, String defaultW, String TaiwanW){
+        miwokTranslation = miwokW;
+        defaultTranslation = defaultW;
+        TWTranslation = TaiwanW;
     }
 
-
-    public void setWords(String miwokW, String engW){
-        miwok = miwokW;
-        eng = engW;
+    public String getDefaultTranslation(){
+        return defaultTranslation;
     }
 
-    public String getEngWords(String miwokW, String engW){
-        return (eng);
+    public String getMiwokTranslation(){
+        return miwokTranslation;
     }
 
-    public String getMiwokWords(String miwokW, String engW){
-        return (miwok);
+    public String getTWTranslation(){
+        return TWTranslation;
     }
 }
